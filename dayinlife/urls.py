3894ac3da385ee0b37f6d6c44fec12c111e59e1b
Christian Brintnall <christianbrintnall@gmail.com>
2018-02-10 14:18:16 -0800

    Add login views/fix server wsgi

--- dayinlife/urls.py
+++ dayinlife/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('auth/', include('login.urls'))
 ]
