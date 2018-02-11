5236ec713c85ed5c1dc3ace8607914f7b18894f2
zefruis <pianoguy197@gmail.com>
2018-02-10 22:23:16 -0800

    Make home into main page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-
 import main.views as view
 
 urlpatterns = [
-	path('', view.index),
-	path('home/', view.home),
+	path('', view.home),
 	path('api/', include('main.api.urls')),
 	path('upload/', view.upload),
 	path('tiles/', view.tiles),
