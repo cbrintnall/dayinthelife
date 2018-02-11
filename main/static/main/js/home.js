6b074d6fa863cc347d450cd68a447d129cfe9771
zefruis <pianoguy197@gmail.com>
2018-02-10 20:52:13 -0800

    Date formatting on slider

--- main/static/main/js/home.js
+++ main/static/main/js/home.js
@@ -2,16 +2,16 @@ var slider = document.getElementById('slider');
 
 slider.margin = '20 auto 30px';
 var slider_range = {
-	'min': [ 0, 1],
-	'max': [24]
+	'min': [ 0, 15],
+	'max': [1440]
 };
 
 noUiSlider.create(slider, {
-    start: [6, 12],
+    start: [360, 720],
     behaviour: 'drag-tap',
     connect: true,
     range: slider_range,
-    tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
+    tooltips: [wNumb({}), wNumb({})],
     steps: 24,
     pips: {
         mode: 'positions',
@@ -30,13 +30,19 @@ $('.noUi-value.noUi-value-horizontal.noUi-value-large').each(function() {
     $(this).html(val);
 });
 
+$(".noUi-tooltip").click(function(){
+    $(".noUi-tooltip").animate({left: '260px'});
+});
+
 function recountVal(val){
     switch(val){
         case 0: return '12:00 AM'; break;
-        case 6: return '6:00 AM'; break;
-        case 12: return '12:00 PM'; break;
-        case 18: return '6:00 PM'; break;
-        case 24: return '12:00 AM'; break;
+        case 360: return '6:00 AM'; break;
+        case 720: return '12:00 PM'; break;
+        case 1080: return '6:00 PM'; break;
+        case 1440: return '12:00 AM'; break;
     }
 }
+
+
 
