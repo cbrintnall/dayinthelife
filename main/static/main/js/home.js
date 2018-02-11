f3b142150f7e7788ff0778e0a8b112781dfbca34
zefruis <pianoguy197@gmail.com>
2018-02-10 18:25:55 -0800

    Update slider in main page

--- main/static/main/js/home.js
+++ main/static/main/js/home.js
@@ -11,13 +11,32 @@ noUiSlider.create(slider, {
     behaviour: 'drag-tap',
     connect: true,
     range: slider_range,
-    tooltips: true,
+    tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
     steps: 24,
     pips: {
         mode: 'positions',
         values: [0, 25, 50, 75, 100],
         density: 4,
         stepped: true,
+        format: wNumb({
+            decimals: 0
+        })
     },
 });
+
+$('.noUi-value.noUi-value-horizontal.noUi-value-large').each(function() {
+    var val = $(this).html();
+    val = recountVal(parseInt(val));
+    $(this).html(val);
+});
+
+function recountVal(val){
+    switch(val){
+        case 0: return '12:00 AM'; break;
+        case 6: return '6:00 AM'; break;
+        case 12: return '12:00 PM'; break;
+        case 18: return '6:00 PM'; break;
+        case 24: return '12:00 AM'; break;
+    }
+}
 
